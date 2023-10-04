b54244c98185431983f28627037cfae6e708c116
Alexandru Donu <donu200000@gmail.com>
2023-10-04 17:01:24 +0300

    better implementation

--- src/App.vue
+++ src/App.vue
@@ -1,199 +1,267 @@
 <script setup lang="ts">
-import { ref, computed } from "vue";
-import ArrowIcon from "./components/ArrowIcon.vue";
+import { ref, computed } from 'vue'
 
-interface Rule {
-  left: string;
-  right: string;
+type Rule = {
+  left: string
+  right: string
 }
+
 const rules = ref<Rule[]>([
-  { left: "S", right: "AB" },
-  { left: "S", right: "BC" },
-  { left: "A", right: "BA" },
-  { left: "A", right: "a" },
-  { left: "B", right: "CC" },
-  { left: "B", right: "b" },
-  { left: "C", right: "AB" },
-  { left: "C", right: "a" },
-]);
-const wordToCheck = ref("baababaababaababaa");
-const rerenderCounter = ref(0);
+  { left: 'S', right: 'AB' },
+  { left: 'S', right: 'BC' },
+  { left: 'A', right: 'BA' },
+  { left: 'A', right: 'a' },
+  { left: 'B', right: 'CC' },
+  { left: 'B', right: 'b' },
+  { left: 'C', right: 'AB' },
+  { left: 'C', right: 'a' },
+])
+const word = ref('baaba')
 
-const Vn = computed(() => {
-  //all uppercase letters from rules, left side and right side
-
-  const Vn = new Set<string>();
-  rules.value.forEach((rule) => {
-    Vn.add(rule.left);
-    rule.right.split("").forEach((char) => {
-      if (char.toUpperCase() === char) {
-        Vn.add(char);
-      }
-    });
-  });
-  return Array.from(Vn);
-});
-
-const Vt = computed(() => {
-  // all characters from rules, except uppercase letters
-  const Vt = new Set<string>();
-  rules.value.forEach((rule) => {
-    rule.right.split("").forEach((char) => {
-      if (char.toLowerCase() === char) {
-        Vt.add(char);
-      }
-    });
-  });
-  return Array.from(Vt);
-});
-
-function addRule() {
-  rules.value.push({ left: "", right: "" });
+function combineSets(set1: Set<string>, set2: Set<string>) {
+  return new Set(Array.from(set1).flatMap(value1 => Array.from(set2).map(value2 => value1 + value2)))
 }
 
-let matrix = ref<any>(new Array(wordToCheck.value.length + 1));
-
-function solve() {
-  //build matrix.value borders
-  // let matrix = ref<any>(new Array(wordToCheck.value.length + 1));
-  for (let i = 0; i < matrix.value.length; i++) {
-    matrix.value[i] = new Array<string>(wordToCheck.value.length + 1);
-  }
-
-  for (let i = 0; i < wordToCheck.value.length; i++) {
-    matrix.value[0][i + 1] = wordToCheck.value[i];
-  }
-  for (let i = 0; i < wordToCheck.value.length; i++) {
-    matrix.value[i + 1][0] = i + 1;
-  }
-  for (let i = 1; i < wordToCheck.value.length + 1; i++) {
-    for (let j = 1; j < wordToCheck.value.length + 1; j++) {
-      matrix.value[i][j] = "";
-    }
-  }
-
-  // fill the first line of matrix.value using Cocke - Younger - Kassami algorithm
-  for (let i = 0; i < wordToCheck.value.length; i++) {
-    for (let j = 0; j < rules.value.length; j++) {
-      if (rules.value[j].right === wordToCheck.value[i]) {
-        matrix.value[1][i + 1] += rules.value[j].left;
-      }
-    }
-  }
-
-  for (let i = 2; i < wordToCheck.value.length + 1; i++) {
-    for (let j = 1; j < wordToCheck.value.length - i + 2; j++) {
-      let nonTerminalCombinations: String[] = [];
-      for (let k = 1; k < i; k++) {
-        const first = matrix.value[k][j].split("");
-        const second = matrix.value[i - k][j + k].split("");
-
-        for (let l = 0; l < first.length; l++) {
-          for (let m = 0; m < second.length; m++) {
-            nonTerminalCombinations.push(first[l] + second[m]);
-          }
-        }
-      }
-
-      //check nonTerminalCombinations
-      for (let k = 0; k < rules.value.length; k++) {
-        for (let l = 0; l < nonTerminalCombinations.length; l++) {
-          if (
-            rules.value[k].right === nonTerminalCombinations[l] &&
-            !matrix.value[i][j].includes(rules.value[k].left)
-          ) {
-            matrix.value[i][j] += rules.value[k].left;
-          }
-        }
-      }
-    }
-  }
-
-  //show the matrix.value
-  console.log(matrix.value);
-  rerenderCounter.value++;
+function mergeSets(...sets: Set<string>[]) {
+  return new Set(Array.from(sets).flatMap(set => Array.from(set)))
 }
+
+function getRulesFor(value: string) {
+  return rules.value.filter(rule => rule.right.includes(value))
+}
+
+function getRulesForSet(set: Set<string>) {
+  return new Set(
+    Array.from(set)
+      .flatMap(getRulesFor)
+      .map(rule => rule.left),
+  )
+}
+
+const table = computed(() => {
+  const firstRow = word.value.split('').map(char => new Set(getRulesFor(char).map(rule => rule.left)))
+
+  const arr = [firstRow]
+
+  for (let i = 1; i < word.value.length; i++) {
+    const row = [] as Set<string>[]
+    for (let j = 0; j < word.value.length - i; j++) {
+      const vertical = arr.map(row => row[j])
+      const diagonal = arr.map((row, index) => row[i + j - index]).reverse()
+
+      const combined = vertical.map((set, index) => combineSets(set, diagonal[index]))
+      row.push(getRulesForSet(mergeSets(...combined)))
+    }
+    arr.push(row)
+  }
+
+  return arr
+})
+
+const axiom = computed(() => rules.value[0]?.left ?? 'Missing axiom')
+const nonterminals = computed(
+  () =>
+    new Set(
+      rules.value
+        .map(rule => rule.left)
+        .join('')
+        .split('')
+        .filter(el => el >= 'A' && el <= 'Z'),
+    ),
+)
+const terminals = computed(() => new Set(rules.value.map(rule => rule.right).filter(el => el >= 'a' && el <= 'z')))
+
+function deleteRule(index: number) {
+  rules.value.splice(index, 1)
+}
+
+const isValid = computed(() => {
+  return (
+    rules.value.length > 0 &&
+    rules.value.every(rule => rule.left.length === 1 && rule.left >= 'A' && rule.left <= 'Z') &&
+    rules.value.every(rule => rule.right.length > 0) &&
+    rules.value.every(rule =>
+      rule.right.split('').every(char => nonterminals.value.has(char) || terminals.value.has(rule.right)),
+    ) &&
+    rules.value.some(rule => rule.right.split('').some(char => terminals.value.has(char)))
+  )
+})
+const wordSatisfiesRules = computed(() => {
+  return table.value[table.value.length - 1][0].has(axiom.value)
+})
 </script>
 
 <template>
-  <v-card class="ma-2">
-    <v-card-title>Algoritmul Cocke - Younger - Kassami</v-card-title>
-  </v-card>
-  <v-card class="d-flex justify-space-between">
-    <v-responsive max-width="400">
-      <v-card>
-        <v-form @submit.prevent>
-          <div v-for="(rule, index) in rules" :key="index" class="d-flex text">
-            <div>{{ index + 1 }}.</div>
-            <v-responsive max-width="50">
-              <v-text-field v-model="rule.left" />
-            </v-responsive>
-            <ArrowIcon />
-            <v-responsive max-width="150">
-              <v-text-field v-model="rule.right" />
-            </v-responsive>
-          </div>
-        </v-form>
-      </v-card>
-    </v-responsive>
-
-    <v-responsive max-width="350">
-      <v-card>
-        <v-card-title>Word to check:</v-card-title>
-        <v-text-field v-model="wordToCheck" @keyup.enter="solve"/>
-        <div class="d-flex">
-          <div class="ma-2">Word length:</div>
-          <div class="ma-2">{{ wordToCheck.length }}</div>
+  <main class="app">
+    <aside>
+      <header>
+        <label>
+          <span>Word:</span>
+          <input v-model="word" type="text" placeholder="word" />
+        </label>
+        <div class="grammar">
+          <h3>Grammar general info:</h3>
+          <h4 v-if="!isValid" class="error">
+            <span>Grammar is invalid</span>
+          </h4>
+          <h4 v-else-if="!wordSatisfiesRules" class="error">
+            <span>Word does not satisfy grammar rules</span>
+          </h4>
+          <h4>
+            <span>Axiom: </span>
+            <span>{{ axiom }}</span>
+          </h4>
+          <h4>
+            <span>Nonterminals: </span>
+            <span>{{ [...nonterminals].join(', ') }}</span>
+          </h4>
+          <h4>
+            <span>Terminals: </span>
+            <span>{{ [...terminals].join(', ') }}</span>
+          </h4>
         </div>
-        <v-btn @click="solve" class="ma-2">Solve</v-btn>
-      </v-card>
-    </v-responsive>
-
-    <v-responsive max-width="350">
-      <v-card>
-        <v-card-title>Grammar general info:</v-card-title>
-        <div class="d-flex">
-          <div class="ma-2">Axiom:</div>
-          <div class="ma-2">{{ rules[0].left }}</div>
+        <div class="controls">
+          <button @click="rules.push({ left: '', right: '' })">Add</button>
         </div>
-        <div class="d-flex">
-          <div class="ma-2">Vn:</div>
-          <div class="ma-2">{{ Vn }}</div>
-        </div>
-        <div class="d-flex">
-          <div class="ma-2">Vt:</div>
-          <div class="ma-2">{{ Vt }}</div>
-        </div>
-      </v-card>
-    </v-responsive>
-  </v-card>
-  <v-card class="d-flex">
-    <v-btn @click="addRule" class="ma-2">Add rule</v-btn>
-    <v-btn @click="rules.pop()" class="ma-2">Remove rule</v-btn>
-  </v-card>
-
-  <v-card>
-    <v-card-title> Results: </v-card-title>
-    <v-table :key="rerenderCounter">
-      <tbody>
-        <tr
-          v-for="(row, indexRow) in matrix"
-          :key="indexRow"
-          :class="{ highlight: indexRow % 2 === 0 }"
-        >
-          <td v-for="(element, index) in row" :key="index">{{ element }}</td>
+      </header>
+      <ol class="rules">
+        <li v-for="(rule, index) in rules" :key="index">
+          <input v-model="rule.left" type="text" />
+          <span>-></span>
+          <input v-model="rule.right" type="text" />
+          <button @click="deleteRule(index)">Delete</button>
+        </li>
+      </ol>
+    </aside>
+    <section>
+      <table v-if="table.length > 0">
+        <tr>
+          <th v-for="(char, index) in word.split('')" :key="index">{{ char }}</th>
         </tr>
-      </tbody>
-    </v-table>
-  </v-card>
+        <tr v-for="(row, rowIndex) in table" :key="rowIndex">
+          <td v-for="(set, setIndex) in row" :key="setIndex">
+            <ul v-if="set.size > 0">
+              <li v-for="value in set" :key="value">{{ value }}</li>
+            </ul>
+            <span v-else>-</span>
+          </td>
+        </tr>
+      </table>
+    </section>
+  </main>
 </template>
 
-<style scoped>
-.text {
-  font-size: 50px;
-}
+<style scoped lang="less">
+.app {
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+  flex-direction: row;
 
-.highlight {
-  background-color: #d9d9d9;
+  aside {
+    flex-grow: 0;
+    flex-shrink: 0;
+    width: 400px;
+    background-color: #eee;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    padding: 16px;
+
+    .error {
+      color: hsl(0, 800%, 60%);
+    }
+
+    header {
+      input {
+        width: 100%;
+        padding: 8px;
+        border: 1px solid #000;
+        border-radius: 4px;
+        margin-bottom: 16px;
+      }
+
+      .controls {
+        border-top: 1px solid #000;
+        margin-top: 8px;
+        padding-top: 8px;
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-end;
+
+        button {
+          padding: 8px 16px;
+          border: 1px solid #000;
+          border-radius: 4px;
+          background-color: #fff;
+          cursor: pointer;
+        }
+      }
+    }
+
+    ol.rules {
+      flex-grow: 1;
+      flex-shrink: 1;
+      overflow: auto;
+      padding: 0;
+      margin: 16px 0 0 0;
+      list-style: none;
+
+      li {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        margin-bottom: 8px;
+
+        input {
+          padding: 8px;
+          border: 1px solid #000;
+          border-radius: 4px;
+          margin-right: 8px;
+          width: 100px;
+        }
+
+        button {
+          padding: 8px 16px;
+          border: 1px solid #000;
+          border-radius: 4px;
+          background-color: #fff;
+          cursor: pointer;
+        }
+      }
+    }
+  }
+
+  section {
+    flex-grow: 1;
+    flex-shrink: 1;
+    background-color: #ddd;
+    overflow: auto;
+    padding: 16px;
+
+    table {
+      border-collapse: collapse;
+      width: 100%;
+
+      tr:nth-child(even) {
+        background-color: #fff;
+      }
+
+      th,
+      td {
+        border: 1px solid #000;
+        padding: 8px;
+
+        ul {
+          margin: 0;
+          padding: 0;
+          list-style: none;
+          display: flex;
+          flex-direction: row;
+        }
+      }
+    }
+  }
 }
 </style>
